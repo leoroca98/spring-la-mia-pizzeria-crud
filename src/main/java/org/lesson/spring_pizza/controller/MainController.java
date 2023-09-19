d80b7573a9b5f6370000ca526778f1dcb4ac3595
leoroca98 <leoroca12345@gmail.com>
2023-09-19 15:45:58 +0200

    inizio esercizio

---
+++ src/main/java/org/lesson/spring_pizza/controller/MainController.java
@@ -0,0 +1,20 @@
+package org.lesson.spring_pizza.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class MainController {
+
+    @GetMapping
+    public String index(){
+        return "home";
+    }
+    @GetMapping("/pizza")
+        public String about() {
+        return "about";
+        }
+
+}
---
+++ src/test/java/org/lesson/spring_pizza/SpringPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.spring_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
