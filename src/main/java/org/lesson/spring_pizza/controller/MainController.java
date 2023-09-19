47f13cd8e0c3f340d4707e824be2ed3e2a23fec5
leoroca98 <leoroca12345@gmail.com>
2023-09-19 17:04:14 +0200

    continuo inizio esercizio pizze

--- src/main/java/org/lesson/spring_pizza/controller/MainController.java
+++ src/main/java/org/lesson/spring_pizza/controller/MainController.java
@@ -1,20 +1,40 @@
 package org.lesson.spring_pizza.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalTime;
 
 @Controller
 @RequestMapping()
 public class MainController {
 
     @GetMapping
-    public String index(){
+    public String index() {
         return "home";
     }
+
     @GetMapping("/pizza")
-        public String about() {
+    public String about() {
         return "about";
+    }
+
+    @Controller
+    @RequestMapping("/")
+    public class GreetingController {
+
+        @GetMapping("/greeting")
+        public String greeting(@RequestParam(name = "name") String name, Model model) {
+            model.addAttribute("name", name);
+            model.addAttribute("time", LocalTime.now());
+            return "greeting";
         }
 
+    }
 }
+
+
