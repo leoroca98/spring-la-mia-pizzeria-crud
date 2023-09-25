f4b661aa4e542aab3e65c6b368a7a955f2f52e24
leoroca98 <leoroca12345@gmail.com>
2023-09-25 17:01:58 +0200

    aggiunto la validation e iniziato la classe Pizza

---
+++ src/main/java/org/lesson/spring_pizza/controller/Pizza.java
@@ -0,0 +1,49 @@
+package org.lesson.spring_pizza.controller;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="db_pizze")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descrizione;
+    private double prezzo;
+
+    // Getter e setter
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+}
